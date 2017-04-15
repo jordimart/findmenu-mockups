4562dc57647fa7f3c5a47902d7942b5a8adaee95
Jordi Martínez Frias <jordimart83@gmail.com>
2017-04-15 04:13:30 +0200

    [DIW-CLI] vista home buscador, carousel previo.

--- src/client/app/home/home.controller.js
+++ src/client/app/home/home.controller.js
@@ -5,11 +5,32 @@
         .module('app.home')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['logger', '$translatePartialLoader'];
+    HomeController.$inject = ['logger', '$translatePartialLoader', '$scope'];
 
-    function HomeController(logger, $translatePartialLoader) {
+    function HomeController(logger, $translatePartialLoader, $scope) {
         var vm = this;
         vm.title = 'Home';
+
+        $scope.images = [{
+            url: 'http://lorempixel.com/100/100/sports'
+        }, {
+            url: 'http://lorempixel.com/100/100/abstract'
+        }, {
+            url: 'http://lorempixel.com/100/100/business'
+        }, {
+            url: 'http://lorempixel.com/100/100/city'
+        }, {
+            url: 'http://lorempixel.com/100/100/technics'
+        }, {
+            url: 'http://lorempixel.com/100/100/cats'
+        }];
+
+        $('.autoplay').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
 
         $translatePartialLoader.addPart('home');
 
